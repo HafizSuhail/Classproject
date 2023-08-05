9207af81486775a5b2e11fae45c6730f53823cc4
Muhammed Suhail <suhailabbas427@gmail.com>
2023-08-05 20:21:46 +0530

    Courselist/add course page added

--- NextLeapAcademy/CourseController.cs
+++ NextLeapAcademy/CourseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NextLeapAcademy.BusinessEntities;
+using NextLeapAcademy.Models;
 
 namespace NextLeapAcademy
 {
@@ -10,6 +11,39 @@ namespace NextLeapAcademy
             var DBCourselist = new Nextleapdbcontex();
             var Courses = DBCourselist.Courses.ToList();
             return View(Courses);
+        }
+
+        public IActionResult AddCourses()
+        {
+            return View();  
+        
+        }
+
+        public IActionResult CourseForm(CourseEditorModel Admininputs)
+        {
+
+            if(ModelState.IsValid)
+            {
+                Course newcourse = new Course();
+
+                newcourse.Title = Admininputs.CourseTitle;
+                newcourse.Duration = Admininputs.Duration;
+                newcourse.Price = Admininputs.Price;
+
+                var DbCourseclass = new Nextleapdbcontex();
+
+                DbCourseclass.Add(newcourse);
+
+                DbCourseclass.SaveChanges();
+
+                return RedirectToAction("CoursesList");
+            }
+            else 
+            {
+                ModelState.AddModelError("", "Course record not Save, please fix errors and save again!");
+                return View("AddCourses", Admininputs); 
+            }
+           
         }
     }
 }
